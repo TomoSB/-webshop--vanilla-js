11b17d3bf203696f07855cf02ad22b37f7ddfe0c
Tomislav <Tomislavvidovic729@gmail.com>
2024-02-27 15:57:37 +0100

    .

---
+++ script.js
@@ -0,0 +1,40 @@
+//let addCart = document.getElementsByClassName("add-cart");
+//let add = document.getElementById("add-cart").innerHTML;
+//console.log(addCart);
+
+
+
+
+let addCart = document.getElementsByClassName("add-cart");
+console.log(addCart);
+
+for (let i = 0; i < addCart.length; i++) {
+    addCart[i].addEventListener("click", addFunction)
+    // console.log(addCart[i]);
+};
+
+function addFunction(e) {
+    let itemList = document.getElementById("item-list");
+    let priceList = document.getElementById("price-list");
+    let value = e.target.parentNode.querySelector('h3').innerHTML;
+    let item = e.target.parentNode.querySelector('h5').innerHTML;
+    itemList.innerHTML += `<li>${item}</li>`;
+    priceList.innerHTML += `<li>${value}</li>`;
+
+
+}
+
+
+/*
+for (let i = 0; i < addCart.length; i++) {
+    // Pass the index i to the addFunction when the event listener is triggered
+    addCart[i].addEventListener("click", function () { addFunction(i); });
+}
+
+function addFunction(index) {
+    // Now you can use the index inside addFunction
+    console.log("Clicked item index:", index);
+}
+*/
+
+
